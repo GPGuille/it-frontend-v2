fd56d10f7b55777ffa0b2d8100f0b81d3f6be097
Guillermo Gonzalez Piqueras <ggonzalez@renr.grupoepi.es>
2025-03-07 12:46:47 +0100

    Login terminado

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import FloatingConfigurator from '@/components/FloatingConfigurator.vue';
+
 import { ref } from 'vue';
 //IMAGENES
 import myImage from '@/assets/images/PrensaIbericaLogo.jpg';
@@ -11,34 +11,39 @@ const checked = ref(false);
 </script>
 
 <template>
-    <FloatingConfigurator />
-    <div 
-    class="flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden"
-    :style="{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }"
->        <div class="flex flex-col items-center justify-center">
-            <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
+
+    <div class="flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden"
+        :style="{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
+        <div class="flex flex-col items-center justify-center">
+            <div
+                style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, #0f5274 10%, rgba(15, 82, 116, 0) 30%)">
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div class="text-center mb-8">
-                        <img :src="myImage" alt="Descripción de la imagen" class="mb-8 w-50 h-32 shrink-0 mx-auto">
-                        
-                        
+                        <img :src="myImage" alt="Logo de Recursos en la Red" class="mb-8 w-70 h-32 shrink-0 mx-auto">
+
+
                     </div>
 
                     <div>
-                        <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" type="text" placeholder="Dirección email" class="w-full md:w-[30rem] mb-8" v-model="email" />
+                        <label for="email1"
+                            class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
+                        <InputText id="email1" type="text" placeholder="Dirección email"
+                            class="w-full md:w-[30rem] mb-8" v-model="email"
+                            @focus="e => e.target.style.borderColor = '#0f5274'"
+                            @blur="e => e.target.style.borderColor = ''" />
 
-                        <label for="password1" class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">Contraseña</label>
-                        <Password id="password1" v-model="password" placeholder="Contraseña" :toggleMask="true" class="mb-4" fluid :feedback="false"></Password>
+                        <label for="password1"
+                            class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">Contraseña</label>
+                        <Password id="password1" v-model="password" placeholder="Contraseña" :toggleMask="true"
+                            class="mb-4" fluid :feedback="false" @focus="e => e.target.style.borderColor = '#0f5274'"
+                            @blur="e => e.target.style.borderColor = ''"></Password>
 
                         <div class="flex items-center justify-between mt-2 mb-8 gap-8">
-                            <div class="flex items-center">
-                                <Checkbox v-model="checked" id="rememberme1" binary class="mr-2"></Checkbox>
-                                <label for="rememberme1">Recuérdame</label>
-                            </div>
-                            <span class="font-medium no-underline ml-2 text-right cursor-pointer text-primary">¿Has olvidado la contraseña?</span>
+
+
                         </div>
-                        <Button label="ACCEDER" class="w-full" as="router-link" to="/"></Button>
+                        <Button label="ACCEDER" class="w-full" as="router-link" to="/"
+                            :style="{ backgroundColor: '#0f5274 !important', color: '#FFFFFF !important', fontWeight: 'bold !important', padding: '0.5rem 1rem !important', borderRadius: '0.375rem !important', borderColor: '#0f5274 !important', borderWidth: '1px' }"></Button>
                     </div>
                 </div>
             </div>
